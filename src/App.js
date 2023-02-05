edc82103c894f11d914bbe673747ba96f4d9bb8e
hoai1506 <luuhoai3107@gmail.com>
2023-02-05 14:36:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const DemoComponent = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Day la tuoi: {props.age}</div>
+    <div>Day la ten: {props.displayName}</div>
     </div>
+  );
+};
+function App() {
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState('Cee');
+
+  const onIncreaseAge = () => {
+    setAge(age + 1);
+  };
+  return (
+    <div className='container'>
+      <div>{age}</div>
+      <div onClick ={onIncreaseAge}>Tang age len 1</div>
+      <DemoComponent age={age} displayName={name} />
+    </div>
+    
   );
 }
 
